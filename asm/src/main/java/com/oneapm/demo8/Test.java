2aae33a482bcc1cf06d094f998245c8e86127757
lilizhao <lilizhaolilizhao@163.com>
2018-11-12 11:26:59 +0800

    添加test部分

---
+++ asm/src/main/java/com/oneapm/demo8/Test.java
@@ -0,0 +1,18 @@
+package com.oneapm.demo8;
+
+public class Test {
+    public static void main(String[] args) {
+        printOne();
+        printOne();
+        printTwo();
+    }
+
+    public static void printOne() {
+        System.out.println("Hello World");
+    }
+
+    public static void printTwo() {
+        printOne();
+        printOne();
+    }
+}
