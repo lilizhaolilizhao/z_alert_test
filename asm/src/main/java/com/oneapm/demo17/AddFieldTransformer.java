99b7187226841abece992f8cc677b8caf644ddbf
lilizhao <lilizhaolilizhao@163.com>
2018-11-17 22:13:44 +0800

    添加类成员

---
+++ asm/src/main/java/com/oneapm/demo17/AddFieldTransformer.java
@@ -0,0 +1,35 @@
+package com.oneapm.demo17;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+
+public class AddFieldTransformer extends ClassTransformer {
+    private int fieldAccess;
+    private String fieldName;
+    private String fieldDesc;
+
+    public AddFieldTransformer(ClassTransformer ct, int fieldAccess,
+                               String fieldName, String fieldDesc) {
+        super(ct);
+        this.fieldAccess = fieldAccess;
+        this.fieldName = fieldName;
+        this.fieldDesc = fieldDesc;
+    }
+
+    @Override
+    public void transform(ClassNode cn) {
+        boolean isPresent = false;
+
+        for (FieldNode fn : cn.fields) {
+            if (fieldName.equals(fn.name)) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        if (!isPresent) {
+            cn.fields.add(new FieldNode(fieldAccess, fieldName, fieldDesc, null, null));
+        }
+        super.transform(cn);
+    }
+}
